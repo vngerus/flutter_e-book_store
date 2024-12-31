a36df52f8240725d4b5e1c2a868b4dcf18b4ae62
angel <angelsmithlgs@gmail.com>
2024-12-31 20:12:30 -0300

    rollup

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,17 +9,49 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      appBar: _buildAppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.grey[200],
+        leading: IconButton(
+          icon: const Icon(Icons.grid_view, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart, color: Colors.black),
+            onPressed: () {},
+          ),
+          const CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Text(
+              "AS",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const SizedBox(width: 16),
+        ],
+      ),
       body: Stack(
         children: [
           Column(
             children: [
-              const SizedBox(height: 16),
-              _buildSearchBar(),
-              const SizedBox(height: 16),
-              Expanded(
-                child: BookCarousel(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: 'Search',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                ),
               ),
+              const SizedBox(height: 16),
+              BookCarousel(),
             ],
           ),
           const Positioned.fill(
@@ -29,49 +61,6 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  AppBar _buildAppBar() {
-    return AppBar(
-      elevation: 0,
-      backgroundColor: Colors.grey[200],
-      leading: IconButton(
-        icon: const Icon(Icons.grid_view, color: Colors.black),
-        onPressed: () {},
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.shopping_cart, color: Colors.black),
-          onPressed: () {},
-        ),
-        const CircleAvatar(
-          backgroundColor: Colors.blue,
-          child: Text(
-            "AS",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        const SizedBox(width: 16),
-      ],
-    );
-  }
-
-  Widget _buildSearchBar() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: TextField(
-        decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.search),
-          hintText: 'Search',
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: Colors.white,
-        ),
       ),
     );
   }
